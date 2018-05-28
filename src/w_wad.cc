ccc63fcc68f86704b864927c2713469c459b2e17
Andrew Apted <ajapted@gmail.com>
2018-05-28 14:45:49 +1000

    wad code : renamed parameter of Backup() method.

--- src/w_wad.cc
+++ src/w_wad.cc
@@ -1208,11 +1208,11 @@ void Wad_file::WriteDirectory()
 }
 
 
-bool Wad_file::Backup(const char *filename)
+bool Wad_file::Backup(const char *new_filename)
 {
 	fflush(fp);
 
-	return FileCopy(PathName(), filename);
+	return FileCopy(PathName(), new_filename);
 }
 
 
