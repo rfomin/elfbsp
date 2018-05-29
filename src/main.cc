32075d09aacff30938da90d581c09e2beeca1d36
Andrew Apted <ajapted@gmail.com>
2018-05-29 23:51:18 +1000

    added "--very-verbose" option, equivalent to "-v -v".

--- src/main.cc
+++ src/main.cc
@@ -632,6 +632,10 @@ int ParseLongArgument(const char *name, int argc, char *argv[])
 	{
 		opt_verbosity += 1;
 	}
+	else if (strcmp(name, "--very-verbose") == 0)
+	{
+		opt_verbosity += 2;
+	}
 	else if (strcmp(name, "--backup") == 0 || strcmp(name, "--backups") == 0)
 	{
 		opt_backup = true;
